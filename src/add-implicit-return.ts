1273b0af36ba79afa977a4c8586c5e9fccaffd17
cynthiacodes <cjnkstudy01@hotmail.com>
2023-07-03 13:54:52 +0100

    wrote one-line implicit returns for add and divide functions with tests

---
+++ src/add-implicit-return.ts
@@ -0,0 +1,12 @@
+/**
+ * Adds together two numbers
+ *
+ * @param firstNum first number to add
+ * @param secondNum second number to add
+ * @returns total of the two numbers
+ */
+const add = (firstNum: number, secondNum: number): number => firstNum + secondNum;
+
+  
+  export default add;
+  
---
+++ src/divide-implicit-return.ts
@@ -0,0 +1,12 @@
+/**
+ * Divides a first number by a second number
+ *
+ * @param firstNum first number (numerator)
+ * @param secondNum second number (denominator)
+ * @returns result of dividing `firstNum` by `secondNum`
+ */
+const divide = (firstNum: number, secondNum: number): number =>  firstNum / secondNum;
+   
+  
+  export default divide;
+  
